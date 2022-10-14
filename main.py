98570e4cd83eaf59aca3f359262dc921f05a5c0c
Apojacsleam <79567165+Apojacsleam@users.noreply.github.com>
2022-10-14 21:09:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from MainGUI import Ui_MainWindow
 from PyQt5 import QtWidgets
+from Database import Create_Database
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    Create_Database()
     mainGUI = Ui_MainWindow()
     mainGUI.show()
     sys.exit(app.exec())
